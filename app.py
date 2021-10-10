202eb7b653b356b4ae29e96637a9cd86cc8a66e8
Clash-02 <yogesh7146@gmail.com>
2021-10-10 22:20:52 +0530

    pre changes

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
-import waitress
 from graph import load_graph, shortest_path
 from flask import Flask, request, render_template, jsonify
-import os
 
 stations = load_graph('static/stn.json')
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-    
+	return render_template('index.html')
+
+@app.route('/trainInfo/')
+def trainInfo():
+	return render_template('trainInfo.html')
 
 @app.route('/api/v1/')
 def api():
-    req = request.args
-    route = shortest_path(stations, req['start'], req['end'])
-    if route is None:
-        return jsonify({'route': 'null'}), 400
-    else:
-        return jsonify({'route': route}), 200
+	req = request.args
+	route = shortest_path(stations, req['start'], req['end'])
+	if route is None:
+		return jsonify({'route': 'null'}), 400
+	else:
+		return jsonify({'route': route}), 200
 
-if __name__ == "__main__":
-     app.debug = False
-     port = int(os.environ.get('PORT', 33507))
-     waitress.serve(app, port=port)
-     app.run
+app.run(debug=True)
